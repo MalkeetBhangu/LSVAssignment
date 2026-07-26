39595255c5a6550e8db3a3bb30ff8e9b4186614f
kamalpreet001 <kamalpreet@logiceasetecno.com>
2026-07-26 11:04:40 +0530

    add loader

--- components/login/index.tsx
+++ components/login/index.tsx
@@ -1,101 +1,139 @@
-import React, { useState } from 'react'
-import { View, Image, Alert } from 'react-native'
-import { useNavigation } from '@react-navigation/native'
-import { Screens } from '@constants/Screens'
-import styles from './styles'
-import { getTexts } from '@translations/TranslationHelper'
-import { DEFAULT_LANGUAGE_CODE } from '@constants/Constants'
-import TextView from '@components/commonUI/TextView'
-import TextInput from '@components/commonUI/TextInput'
-import Button from '@components/commonUI/Button'
-import { signInWithGoogle } from 'config/google'
-import Waves from '@components/commonUI/Waves'
-import { GoogleIcon, FacebookIcon } from '@components/commonUI/SocialIcons'
+import React, { useState } from "react";
+import { View, Image, Alert, ActivityIndicator } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { Screens } from "@constants/Screens";
+import styles from "./styles";
+import { getTexts } from "@translations/TranslationHelper";
+import { DEFAULT_LANGUAGE_CODE } from "@constants/Constants";
+import TextView from "@components/commonUI/TextView";
+import TextInput from "@components/commonUI/TextInput";
+import Button from "@components/commonUI/Button";
+import { signInWithGoogle } from "config/google";
+import Waves from "@components/commonUI/Waves";
+import { GoogleIcon, FacebookIcon } from "@components/commonUI/SocialIcons";
+import Loader from "@components/loader";
 
 const Login: React.FC = () => {
-    const navigation = useNavigation<any>()
-    const t = getTexts(DEFAULT_LANGUAGE_CODE)
-    const [emailOrPhone, setEmailOrPhone] = useState('')
-    const [password, setPassword] = useState('')
+  const navigation = useNavigation<any>();
+  const t = getTexts(DEFAULT_LANGUAGE_CODE);
+  const [emailOrPhone, setEmailOrPhone] = useState("");
+  const [password, setPassword] = useState("");
+  const [googleLoading, setGoogleLoading] = useState(false);
+  const handleLogin = () => {
+    navigation.navigate(Screens.MAIN_TABS);
+  };
 
-    const handleLogin = () => {
-        navigation.navigate(Screens.MAIN_TABS)
-    }
+  const handleForgotPassword = () => {
+    // Handle forgot password logic
+  };
 
-    const handleForgotPassword = () => {
-        // Handle forgot password logic
-    }
+  const handleGoogleLogin = async () => {
+    if (googleLoading) return;
 
-    const handleGoogleLogin = async() => {
-        // Handle Google login
-         try {
+    setGoogleLoading(true);
+
+    try {
       const userCredential = await signInWithGoogle();
 
       console.log(userCredential.user);
+      navigation.navigate(Screens.MAIN_TABS);
 
-      Alert.alert(
-        'Success',
-        `Welcome ${userCredential.user.displayName}`,
-      );
+      // Alert.alert(
+      //   'Success',
+      //   `Welcome ${userCredential.user.displayName}`,
+      // );
     } catch (error) {
       console.log(error);
-            const message = error instanceof Error ? error.message : 'Google Login Failed';
-            Alert.alert('Google Login Failed', message);
-    }
-    }
 
-    const handleFacebookLogin = () => {
-        // Handle Facebook login
+      const message =
+        error instanceof Error ? error.message : "Google Login Failed";
+
+      Alert.alert("Google Login Failed", message);
+    } finally {
+      setGoogleLoading(false);
     }
+  };
 
-    const handleSignUp = () => {
-        // Handle Sign Up navigation
-    }
+  const handleFacebookLogin = () => {
+    // Handle Facebook login
+  };
 
-    return (
-        <View style={styles.container}>
-            <View style={styles.content}>
-                <View style={styles.topSection}>
-                    <View style={styles.imageWrapper}>
-                        <Image source={require('@assets/images/logo.png')} style={styles.imageStyle} />
-                    </View>
-                    <TextView style={styles.welcomeTitle} text={t.login.welcomeText} />
-                    <TextView style={styles.welcomeSubtitle} text={t.login.welcomeSubtitle} />
-                </View>
+  const handleSignUp = () => {
+    // Handle Sign Up navigation
+  };
 
-                <View style={styles.middleSection}>
-                    <TextInput label={t.login.emailOrPhoneLabel} placeholder={t.login.emailOrPhonePlaceholder} value={emailOrPhone} onChangeText={setEmailOrPhone} keyboardType="email-address" autoCapitalize="none" />
-                    <TextInput label={t.login.passwordLabel} placeholder={t.login.passwordPlaceholder} value={password} onChangeText={setPassword} isPassword />
-                    <View style={styles.forgotPasswordContainer}>
-                        <TextView style={styles.forgotPasswordText} text={t.login.forgotPassword} onPress={handleForgotPassword} />
-                    </View>
-                    <Button title={t.login.loginButton} onPress={handleLogin} />
-                </View>
+  return (
+    <View style={styles.container}>
+      <View style={styles.content}>
+        <View style={styles.topSection}>
+          <View style={styles.imageWrapper}>
+            <Image
+              source={require("@assets/images/logo.png")}
+              style={styles.imageStyle}
+            />
+          </View>
+          <TextView style={styles.welcomeTitle} text={t.login.welcomeText} />
+          <TextView
+            style={styles.welcomeSubtitle}
+            text={t.login.welcomeSubtitle}
+          />
+        </View>
+
+        <View style={styles.middleSection}>
+          <TextInput
+            label={t.login.emailOrPhoneLabel}
+            placeholder={t.login.emailOrPhonePlaceholder}
+            value={emailOrPhone}
+            onChangeText={setEmailOrPhone}
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+          <TextInput
+            label={t.login.passwordLabel}
+            placeholder={t.login.passwordPlaceholder}
+            value={password}
+            onChangeText={setPassword}
+            isPassword
+          />
+          <View style={styles.forgotPasswordContainer}>
+            <TextView
+              style={styles.forgotPasswordText}
+              text={t.login.forgotPassword}
+              onPress={handleForgotPassword}
+            />
+          </View>
+          <Button title={t.login.loginButton} onPress={handleLogin} />
+        </View>
+      </View>
+
+      <View style={styles.bottomSection}>
+        <Waves>
+          <View style={styles.waveContent}>
+            <View style={styles.dividerContainer}>
+              <View style={styles.dividerLine} />
+              <TextView
+                style={styles.dividerText}
+                text={t.login.orContinueWith}
+              />
+              <View style={styles.dividerLine} />
             </View>
 
-            <View style={styles.bottomSection}>
-                <Waves>
-                    <View style={styles.waveContent}>
-                        <View style={styles.dividerContainer}>
-                            <View style={styles.dividerLine} />
-                            <TextView style={styles.dividerText} text={t.login.orContinueWith} />
-                            <View style={styles.dividerLine} />
-                        </View>
-
-                        <View style={styles.socialButtonsContainer}>
+            <View style={styles.socialButtonsContainer}>
                             <Button title={t.login.continueWithGoogle} variant="social" leftIcon={<GoogleIcon size={22} />} onPress={handleGoogleLogin} style={styles.socialButtonMargin} textStyle={{ fontWeight: 400 }} />
                             <Button title={t.login.continueWithFacebook} variant="social" leftIcon={<FacebookIcon size={22} />} onPress={handleFacebookLogin} textStyle={{ fontWeight: 400 }} />
-                        </View>
+            </View>
 
-                        <View style={styles.signUpContainer}>
+            <View style={styles.signUpContainer}>
                             <TextView style={styles.signUpPrompt} text={`${t.login.dontHaveAccount} `} />
                             <TextView style={styles.signUpLink} text={t.login.signUp} onPress={handleSignUp} />
-                        </View>
-                    </View>
-                </Waves>
             </View>
-        </View>
-    )
-}
+          </View>
+        </Waves>
+      </View>
+        <Loader visible={googleLoading} />
 
-export default Login
+    </View>
+  );
+};
+
+export default Login;
